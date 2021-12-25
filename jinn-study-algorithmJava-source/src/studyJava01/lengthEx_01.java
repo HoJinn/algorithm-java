50b2bf2f20d00303d609eac920ce0bc9212fb167
Jinn <quizho93@gmail.com>
2021-12-25 10:02:46 +0900

    20211225 lengthEx_01 예제 완성

---
+++ jinn-study-algorithmJava-source/src/studyJava01/lengthEx_01.java
@@ -0,0 +1,9 @@
+package studyJava01;
+
+public class lengthEx_01 {
+
+	public static void main(String[] args) {
+		String[] arrStr = {"홍길동" , "이순신" ,"김유신","신사임당" } ;
+		System.out.println("배열의 길이 : " +arrStr.length); // 결과값 : 배열의 길이 : 4 
+	}
+}
