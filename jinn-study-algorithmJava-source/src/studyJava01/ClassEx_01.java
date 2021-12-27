f0a9a1cb8df05782359c026d23355e7e56065b7a
Jinn <quizho93@gmail.com>
2021-12-27 19:19:26 +0900

    20211227 Class 연습

---
+++ jinn-study-algorithmJava-source/src/studyJava01/ClassEx_01.java
@@ -0,0 +1,21 @@
+package studyJava01;
+
+public class ClassEx_01 {
+
+	public static void main(String[] args) {
+		
+		ClassB cd = new ClassB(); 
+		cd.print(); 
+		
+	}
+	
+	public void print() { 
+		System.out.println("여기는 ClassA");
+	}
+}
+class ClassB{ 
+	void print() { 
+		System.out.println("여기는 ClassB");
+	}
+}
+
