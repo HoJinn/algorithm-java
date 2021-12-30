80beec9466fb829a8a16750d9a836fd573d08753
Jinn <quizho93@gmail.com>
2021-12-30 18:37:42 +0900

    20211230 poly 예제 공부

---
+++ jinn-study-algorithmJava-source/src/studyJava01/polyEx/Amd.java
@@ -0,0 +1,8 @@
+package studyJava01.polyEx;
+
+public class Amd extends GraphicCard{
+	
+	public void process() { 
+		System.out.println("AMD 그래픽 처리");
+	}
+}
---
+++ jinn-study-algorithmJava-source/src/studyJava01/polyEx/GraphicCard.java
@@ -0,0 +1,9 @@
+package studyJava01.polyEx;
+
+public class GraphicCard {
+	int memory;
+	
+	public void process() { 
+		System.out.println("그래픽 처리");
+	}
+}
