297e566e8cd5cef9f0c619ed2ad65905fd5a3789
Jinn <quizho93@gmail.com>
2021-12-31 12:53:35 +0900

    20211231 클래스 상속관계 동물예제 완성

---
+++ jinn-study-algorithmJava-source/src/studyJava01/polyEx02/Lion.java
@@ -0,0 +1,12 @@
+package studyJava01.polyEx02;
+
+public class Lion extends Animal {
+	
+	Lion(String type, String name) { 
+		super(type, name); 
+	}
+	
+	void sleep() { 
+		System.out.println(this.name + "은(는) 숲속에서 잠을 잔다.");
+	}
+}
