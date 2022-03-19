47626c1e8d953b55c832beafa8d0899efe47f56b
Jinn <quizho93@gmail.com>
2022-03-19 23:26:15 +0900

    20220319 read(byte[]b, int off , int len)메소드를 이용해 전체출력 예제 완성

---
+++ jinn-study-algorithmJava-source/src/studyJava13/IO/FileInputStreamEx_03.java
@@ -0,0 +1,27 @@
+package studyJava13.IO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileInputStreamEx_03 {
+	
+	/* 
+	 * read(byte[]b, int off , int len) 메서드를 이용하여 예제를 만들어보자.
+	 */
+	public static void main(String[] args) {
+		
+		try {
+			FileInputStream fis = 
+					new FileInputStream("src/studyJava13/IO/FileInputStreamEx_01.java");
+			int data = 0; 
+			byte[] buf = new byte[fis.available()];
+			while ((data=fis.read(buf, 0, buf.length)) != -1) { 
+				System.out.println(new String (buf, 0, data));
+			}
+		} catch (IOException e) {
+			
+		}
+		
+	}
+
+}
