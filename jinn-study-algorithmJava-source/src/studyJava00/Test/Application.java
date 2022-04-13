a395b8cdba4b0cdeab3a7d0ce5cce043badec735
Jinn <quizho93@gmail.com>
2022-04-13 12:50:04 +0900

    20220413 정수1부터 10까지 총 합하는 예제 완성

--- jinn-study-algorithmJava-source/src/studyJava00/Test/Application.java
+++ jinn-study-algorithmJava-source/src/studyJava00/Test/Application.java
@@ -5,9 +5,14 @@ public class Application {
 	public static void main(String[] args) {
 		
 //		System.out.println(excerciseInt03.Coin());
-		System.out.println(excerciseInt03.rockPaperScissors());
-		System.out.println(excerciseInt03.randomAlphabet(8));
-		System.out.println(excerciseInt03.LowerAlphabet(8));
+//		System.out.println(excerciseInt03.rockPaperScissors());
+//		System.out.println(excerciseInt03.randomAlphabet(8));
+//		System.out.println(excerciseInt03.LowerAlphabet(8));
+		
+		Calculator calculator = new Calculator(); 
+		
+		System.out.println("1부터 10까지의 합 : "  + calculator.sum1to10());
+	
 	}
 
 }
---
+++ jinn-study-algorithmJava-source/src/studyJava00/Test/Calculator.java
@@ -0,0 +1,20 @@
+package studyJava00.Test;
+
+public class Calculator {
+	
+	/* 
+	 * 1부터 10까지의 총 합을 반환하는 메소드를 만들어 보자 
+	 */
+	
+	public int sum1to10() { 
+		int sum = 0; 
+		
+		for(int i = 0; i <=10; i++) { 
+			sum += i; 
+		}
+		return sum; 
+	}
+	
+	
+
+}
