2522d97ff3678bd647dc1d80627a41268b0dd91c
Jinn <quizho93@gmail.com>
2022-05-24 19:24:24 +0900

    20220524 ArrayList 간단한 값추가하기

---
+++ jinn-study-algorithmJava-source/src/studyJava08/Collection/ArrayListEx_01.java
@@ -0,0 +1,24 @@
+package studyJava08.Collection;
+
+import java.util.ArrayList;
+
+public class ArrayListEx_01 {
+	/* 
+	 * ArrayList 값을 추가해보자. 
+	 * 
+	 * ArrayList의 값 추가하기 위해 add()메서드를 사용합니다. 
+	 * 
+	 * add(Object): ArrayList의 마지막에 데이터를 추가합니다. 
+	 * add(int index,Object) : ArrayList의 index에 데이터를 추가합니다. 
+	 */
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<>();
+		
+		al.add("Hello"); 
+		al.add("Hello"); 
+		al.add(1,"World"); 
+		
+		System.out.println(al);
+	}
+
+}
