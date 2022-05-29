16ea97a43892cebf06c93523812f1ae55ebc3a1f
Jinn <quizho93@gmail.com>
2022-05-29 23:20:42 +0900

    20220529 ArrayList 값 삭제하기 완성

--- jinn-study-algorithmJava-source/src/studyJava08/Collection/ArrayListEx_01.java
+++ jinn-study-algorithmJava-source/src/studyJava08/Collection/ArrayListEx_01.java
@@ -21,9 +21,25 @@ public class ArrayListEx_01 {
 		//값 변경하기 
 		System.out.println("초기값" + al);
 		
-		al.set(1, "World"); 
+//		al.set(1, "World");//변경하기  
 		
-		System.out.println("변경된 값 : " + al);
+//		System.out.println("변경된 값 : " + al);
+		
+		//값 삭제하기 
+		/* 
+		 * clear(): ArrayList의 모든 값을 삭제할 때 사용 된다. 
+		 * remove() : 값을 하나씩 제거할때 사용한다. 
+		 */
+		
+		al.remove("Hello"); 
+		System.out.println("1번 삭제 : " + al);
+		
+		al.remove(1); 
+		System.out.println("2번 삭제 : " + al);
+		
+		al.clear(); 
+		System.out.println("3번 삭제 : " + al);
+		
 	}
 
 }
