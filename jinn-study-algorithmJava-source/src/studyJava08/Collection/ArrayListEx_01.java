8743c77b46e0720eb1275f441fd584a0305d4fbd
HoJinChoi <quizho93@gmail.com>
2022-06-03 13:07:42 +0900

    20220603 ArrayList 값출력하기 get,Iterator 두가지 방법 공부

--- jinn-study-algorithmJava-source/src/studyJava08/Collection/ArrayListEx_01.java
+++ jinn-study-algorithmJava-source/src/studyJava08/Collection/ArrayListEx_01.java
@@ -1,6 +1,7 @@
 package studyJava08.Collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListEx_01 {
 	/* 
@@ -41,7 +42,25 @@ public class ArrayListEx_01 {
 
 		//ArrayList 크기 구하기 
 		//ArrayList의 크기를 구하는 방법은 size()메서드를 사용하여 구할수 있습니다. 
-		System.out.println("ArrayList의 크기 : " + al.size());
+//		System.out.println("ArrayList의 크기 : " + al.size());
+		
+		//ArrayList 값 출력하기 
+		//ArrayList의 결과를 출력하는 방법에는 get(int Index)메서드가 있습니다. 
+		//전부 출력하고 싶다면 for문과 향상된for문을 사용하여 출력을 할 수 있습니다. (Iterator를 사용하여 출력을 할수도 있다.) 
+		
+		for (int i=0; i<al.size(); i++) { 
+			System.out.print(al.get(i)+" ");
+		}
+		System.out.println();
+		
+		for (String str : al) 
+			System.out.print(str + " ");
+		
+		System.out.println();
+		
+		Iterator iter = al.iterator(); 
+		while(iter.hasNext()) { 
+			System.out.print(iter.next()+ " ");
+		}
 	}
-
 }
